a43259a2fd4069b0daed0abd14291cb24e9744e5
Unknown <gyomb.graphics@gmail.com>
2018-10-19 23:50:06 +0200

    Countdown fix

--- src/renderer/components/CountdownDisplay.vue
+++ src/renderer/components/CountdownDisplay.vue
@@ -1,9 +1,8 @@
 <template>
   <div class="countdown-display tags has-addons">
     <span class="tag is-large is-danger" v-if="this.isOut">{{this.isOut}}</span>
-    <span class="tag is-large is-warning" v-if="this.rweeks">{{this.rweeks}} week{{this.rweeks>1?'s':''}}</span>
-    <span class="tag is-large is-warning" v-if="this.rdays">{{this.rdays}} day{{this.rdays>1?'s':''}}</span>
-    <span class="tag is-large is-warning" v-if="!this.rdays && this.rhours">{{this.rhours}} hour{{this.rhours>1?'s':''}}</span>
+    <span class="tag is-large is-warning" v-if="this.rtime.months">{{this.rtime.months}} month{{this.rtime.months>1?'s':''}}</span>
+    <span class="tag is-large is-warning" v-if="this.rtime.days">{{this.rtime.days}} day{{this.rtime.days>1?'s':''}}</span>
   </div>
 </template>
 
@@ -14,39 +13,60 @@ export default {
     enddate: String
   },
   computed: {
+    end () {
+      return new Date(this.enddate).getDate()
+      // return new Date(this.enddate).getTime() / (1000 * 60 * 60 * 24)
+    },
+    now () {
+      return new Date().getDate()
+      // return new Date().getTime() / (1000 * 60 * 60 * 24)
+    },
     rtime () {
-      let today = new Date().getTime()
-      let end = new Date(this.enddate).getTime()
-      return end - today
-    },
-    rweeks () {
-      return this.rtime > 0 ? Math.floor(this.rtime / (1000 * 60 * 60 * 24 * 7)) : 0
-    },
-    rdays () {
-      if (this.rtime < 0) {
-        return 0
+      let today = new Date()
+      let end = new Date(this.enddate)
+      let remaining = {
+        years: end.getFullYear() - today.getFullYear()
       }
-      let days = Math.floor(this.rtime / (1000 * 60 * 60 * 24))
-      if (this.rweeks > 0) {
-        days = days % (this.rweeks * 7)
+      function diffDateParts (part, today, end) {
+        switch (part) {
+          case 'month': return end.getMonth() - today.getMonth()
+          case 'day':
+          default: return end.getDate() - today.getDate()
+        }
       }
-      return days
-    },
-    rhours () {
-      if (this.rtime < 0) {
-        return 0
+      function addDateParts (part, today, end) {
+        switch (part) {
+          case 'month': return 12 - today.getMonth() + end.getMonth()
+          case 'day':
+          default: return new Date(today.getFullYear(), today.getMonth() + 1, 0).getDate() - today.getDate() + end.getDate()
+        }
       }
-      let hours = Math.floor(this.rtime / (1000 * 60 * 60))
-      if (this.rdays > 0) {
-        hours = hours % (this.rdays * 24)
+      if (remaining.years >= 0) {
+        if (remaining.years === 0) {
+          remaining.months = diffDateParts('month', today, end)
+        } else {
+          remaining.years--
+          remaining.months = addDateParts('month', today, end)
+        }
+        if (remaining.months >= 0) {
+          if (remaining.months === 0) {
+            remaining.days = diffDateParts('day', today, end)
+          } else {
+            remaining.months--
+            remaining.days = addDateParts('day', today, end)
+          }
+          if (remaining.days >= 0) {
+            return remaining.days === 0 ? 0 : remaining
+          }
+        }
       }
-      return hours
+      return -1
     },
     isOut () {
       var msg = ''
       if (this.rtime <= 0) {
-        msg = 'Out'
-        msg += this.rtime >= -(1000 * 60 * 60 * 24) ? ' TODAY!' : ''
+        msg = 'Released'
+        msg += this.rtime === 0 ? ' TODAY!' : ''
       }
       return msg
     }
